b1199b6b48e37c345fd8d4db00826a085a48e40f
inzerrr <95843791+inzerrr@users.noreply.github.com>
2021-11-16 16:10:02 +0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+
+<html lang=ru>
+
+<head>
+	<title>О проекте</title>
+	<meta charset= "utf-8">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+<script src="/assets/js/google-translate.js"></script>
+<script src="//translate.google.com/translate_a/element.js?cb=TranslateInit"></script>
+</head>
+
+<body>
+	<?php include ('views/partials/header.php');?>
+	<div id = "main">
+	<h1>Данный интернет-ресурс был сделан командой студентов ЮУрГУ</h1>
+	<div style="width: 70%; height: 30%; border:3px solid; margin:auto;"><center>тут наша фотка</center></div><br>
+	<center>Тут какая-нибудь инфа про нас</center>
+	</div>
+	<?php include ('views/partials/footer.php');?>
+</body>
+
+</html>
