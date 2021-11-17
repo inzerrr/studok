f9a7496f01f7b1ade668d100b256c15dc5c5deac
inzerrr <95843791+inzerrr@users.noreply.github.com>
2021-11-17 12:57:29 +0500

    Add files via upload

---
+++ contacts.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE HTML>
+<html lang=ru>
+<head>
+	<title>Контакты</title>
+	<meta charset= "utf-8">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+<script src="/assets/js/google-translate.js"></script>
+<script src="//translate.google.com/translate_a/element.js?cb=TranslateInit"></script>
+</head>
+<body>
+	<?php include ('views/partials/header.php');?>
+	<div id = "main">
+		<p id = "kontdanie">Контактные данные:</p><br>
+		<img id = "img5" src="assets\img\mail.png" alt="mail"> <p>Почта:</p><br> 
+		<em id = "contmail">studo0k@inbox.ru</em><br>
+		<img id = "img6" src="assets\img\telephone.png" alt="telephone"> <p>Телефон:</p><br> 
+		<em id = "phone">8-888-888-88-88</em><br>
+		<img id = "img7" src="assets\img\instagram.png" alt="instagram">
+		<p>Instagram accounts:</p><br>
+		<a id = "inst1" href="https://www.instagram.com/d_dorothy_y/">d_dorothy_y</a><br>
+		<a id = "inst2" href="https://www.instagram.com/nikky_nadin/">nikky_nadin</a><br>
+		<a id = "inst3" href="https://www.instagram.com/sruatall/">sruatall</a><br>
+		<a id = "inst4" href="https://www.instagram.com/inzerrr/">inzerrr</a>		
+	</div>
+	<?php include ('views/partials/footer.php');?>
+</body>
+</html>
