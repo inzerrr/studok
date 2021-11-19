1f14e2d8c204707eca875c26c114126798ba033b
Dashulityy <95776265+Dashulityy@users.noreply.github.com>
2021-11-19 16:23:47 +0500

    Create auth.php

---
+++ Модели/auth.php
@@ -0,0 +1,10 @@
+﻿<?php
+	function GetUserInfo($connect,$id){
+		$query = sprintf("SELECT * FROM users WHERE id=%d",(int)$id);
+        $result = mysqli_query($connect, $query);
+        if(!$result){
+            die(mysqli_error($connect));
+        }
+        return mysqli_fetch_assoc($result);
+	}
+?>
