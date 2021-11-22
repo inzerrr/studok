a286994a85eb65ad65ba36ffa1cd20e4b49b2660
sruatall <71919514+sruatall@users.noreply.github.com>
2021-11-22 19:15:17 +0500

    Create header.php

---
+++ views/partials/header.php
@@ -0,0 +1,17 @@
+<header>
+		<img id = "img1" src="assets/img/S.png" alt="S">
+		<p id = "tudok">t u d o k</p>
+		<p id = "glav"><a href="/">Главная</a></p>
+		<p id = "kont"><a href="/?go=contacts">Контакты</a></p>
+		<p id = "about"><a href="/?go=about">О проекте</a></p>
+		<p id = "vhod"><a href="/?go=login"><img id = "img3" src="assets/img/account.png" alt="account">Авторизация</a></p>
+	 <link rel="shortcut icon" type="image/png" href="assets/img/favicon.ico"/>
+		<link rel="shortcut icon" type="image/png" href="assets/img/favicon.ico"/>
+		<link rel="stylesheet" media="screen and (orientation: portrait) and 
+(max-width:640px), projection" "href=»style.css" />
+<div class="language">
+   <b data-google-lang="ru">ru</b>
+   <b data-google-lang="uk">ua</b>
+   <b data-google-lang="en">en</b>
+</div>
+</header>
