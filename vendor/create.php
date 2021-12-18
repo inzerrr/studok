56a3357f9e72ede31d30a3c1cc71ced005ee7642
Ярослав <yarikterri@yandex.ru>
2021-12-18 15:05:27 +0500

    изменеиене

--- vendor/create.php
+++ vendor/create.php
@@ -4,7 +4,7 @@
 
 
 /*
- * Подключаем файл для получения соединения к базе данных (PhpMyAdmin, MySQL)
+ * Подключаем файл для полученияffffff соединения к базе данных (PhpMyAdmin, MySQL)
  */
 
 require_once '../config/connect.php';
